bf97b098b54363465c73d13d2acd4be005ce9b85
Wesley Bliss <wesley.bliss@gmail.com>
2016-09-15 02:14:23 -0400

    Fixed the hostname on the Express listener.

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ var port = process.env.PORT || 3001;
 app.use( morgan('dev') );
 app.use( express.static( path.join( __dirname, 'public' ) ) );
 
-app.listen( port, function() {
+app.listen( port, '0.0.0.0', function() {
     console.log( 'App listening on', port );
 });
